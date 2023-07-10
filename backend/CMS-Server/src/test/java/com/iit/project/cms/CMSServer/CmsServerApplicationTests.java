2fb9f4431618ee9c6fd3a60443a1abd58da1d7f4
ghliu <ghliu@vw-mobvoi.com>
2023-07-10 16:03:12 +0800

    init project

---
+++ backend/CMS-Server/src/test/java/com/iit/project/cms/CMSServer/CmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.project.cms.CMSServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
