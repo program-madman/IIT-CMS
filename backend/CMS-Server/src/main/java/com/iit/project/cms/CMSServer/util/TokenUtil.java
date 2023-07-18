4391b57b0416695ea440d5d221fe5faf60a0e27d
liuguanghui.2020 <liuguanghui.2020@gmail.com>
2023-07-18 15:20:23 +0800

    Set token expires 30 days

--- backend/CMS-Server/src/main/java/com/iit/project/cms/CMSServer/util/TokenUtil.java
+++ backend/CMS-Server/src/main/java/com/iit/project/cms/CMSServer/util/TokenUtil.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.Gson;
+import com.iit.project.cms.CMSServer.common.Symbol;
 import com.iit.project.cms.CMSServer.entity.User;
 
 import java.util.Date;
@@ -18,7 +19,7 @@ public class TokenUtil {
     public static String generateToken(User user) {
         Algorithm algorithm = Algorithm.HMAC256(KEY_SECRET);
         Date now = new Date();
-        Date expireTime = new Date(now.getTime() + 2592000000L);
+        Date expireTime = new Date(now.getTime() + TOKEN_EXPIRES_TIME);
         return JWT.create()
                 .withIssuer(KEY_ISSUE)
                 .withIssuedAt(now)
@@ -32,8 +33,13 @@ public class TokenUtil {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(KEY_SECRET)).build();
             DecodedJWT decodedJWT = verifier.verify(jwtToken);
             // 验证是否在有效期内
-            return !decodedJWT.getExpiresAt().before(new Date()); // JWT 已过期
-            // 可以根据需要验证其他自定义的字段
+            // 验证是否在有效期内
+            if (decodedJWT.getExpiresAt().before(new Date())) {
+                return false; // JWT 已过期
+            }
+            // 验证uid是不是同一个人
+            //todo
+            return true;
         } catch (JWTVerificationException ex) {
             return false; // JWT 验证失败
         }
